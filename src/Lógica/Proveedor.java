cdaeb73f76cebee525c397edb634430f46f1799d
Andrea-Reyes <karen.reyeslem@gmail.com>
2022-11-17 23:54:21 -0600

    clase puestos y proveedores

--- src/Lógica/Proveedor.java
+++ src/Lógica/Proveedor.java
@@ -5,5 +5,35 @@ package Lógica;
  * @author 50236
  */
 public class Proveedor {
-    
+    private int idproveedor;
+    private String nombreProveedor;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(int idproveedor, String nombreProveedor) {
+        this.idproveedor = idproveedor;
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public int getIdproveedor() {
+        return idproveedor;
+    }
+
+    public void setIdproveedor(int idproveedor) {
+        this.idproveedor = idproveedor;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" + "idproveedor=" + idproveedor + ", nombreProveedor=" + nombreProveedor + '}';
+    }
 }
--- src/Lógica/Puesto.java
+++ src/Lógica/Puesto.java
@@ -5,5 +5,35 @@ package Lógica;
  * @author 50236
  */
 public class Puesto {
-    
+    private int idpuesto;
+    private String puesto;
+
+    public Puesto() {
+    }
+
+    public Puesto(int idpuesto, String puesto) {
+        this.idpuesto = idpuesto;
+        this.puesto = puesto;
+    }
+
+    public int getIdpuesto() {
+        return idpuesto;
+    }
+
+    public void setIdpuesto(int idpuesto) {
+        this.idpuesto = idpuesto;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    @Override
+    public String toString() {
+        return "Puesto{" + "idpuesto=" + idpuesto + ", puesto=" + puesto + '}';
+    }
 }
