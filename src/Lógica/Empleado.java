52ff243c1a0e1064e5c4ff468edf4eb4960565db
Pablo Javier Alemán Velásquez <paleman-2019222@kinal.edu.gt>
2022-11-17 23:15:42 -0600

    creacion de clase empleado

--- src/Lógica/Empleado.java
+++ src/Lógica/Empleado.java
@@ -2,21 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package GUI;
+package Lógica;
 
 /**
  *
  * @author PABLO ALEMAN
  */
-public class Ejemplo {
-    //este es un ejemplo por Pablo
-    //comentario de karen
-    
-    String nombre;
-    String prueba;
-    String prueba2;//solo es una prueba
-    
-    
-    
+public class Empleado {
     
 }
