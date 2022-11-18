3a047a0edda8ffa10740145b8e09738d167d094e
Raquel-Reyes <evelinreyeslem@gmail.com>
2022-11-18 14:13:31 -0600

    Interfaz producto

--- src/Lógica/Platillo.java
+++ src/Lógica/Platillo.java
@@ -1,9 +1,49 @@
 package Lógica;
 
+import java.util.logging.Logger;
+
 /**
  *
  * @author 50236
  */
 public class Platillo {
+    private int idplatillo;
+    private String nombrePlatillo;
+    private double precioPlatillo;
+
+    public Platillo(int idplatillo, String nombrePlatillo, double precioPlatillo) {
+        this.idplatillo = idplatillo;
+        this.nombrePlatillo = nombrePlatillo;
+        this.precioPlatillo = precioPlatillo;
+    }
+
+
+    public Platillo() {
+    }
+
+    public int getIdplatillo() {
+        return idplatillo;
+    }
+
+    public void setIdplatillo(int idplatillo) {
+        this.idplatillo = idplatillo;
+    }
+
+    public String getNombrePlatillo() {
+        return nombrePlatillo;
+    }
+
+    public void setNombrePlatillo(String nombrePlatillo) {
+        this.nombrePlatillo = nombrePlatillo;
+    }
+
+    public double getPrecioPlatillo() {
+        return precioPlatillo;
+    }
+
+    public void setPrecioPlatillo(double precioPlatillo) {
+        this.precioPlatillo = precioPlatillo;
+    }
+
     
 }
